62365892b618f8f26af80e4cef3f001a10220860
Mohammed Shadab <mdshadab0500@gmail.com>
2025-08-22 12:56:38 +0530

    webasg.tf

--- webasg.tf
+++ webasg.tf
@@ -16,7 +16,7 @@ resource "aws_launch_template" "swiggy-web-template" {
   name_prefix   = "swiggy-web-template"
   image_id      = "ami-0861f4e788f5069dd"
   instance_type = "t2.micro"
-  key_name      = "Auto.pem"
+  key_name      = "Auto"
   network_interfaces {
     associate_public_ip_address = true
     security_groups             = [aws_security_group.swiggy-ec2-asg-sg.id]
